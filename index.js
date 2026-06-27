c3c249a24a13512645dff73a2f11f54b8196e330
jubayer50 <aujubayer1@gmail.com>
2026-06-27 15:37:05 +0600

    add expense post method applied

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_DB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("expense-tracker");
+    const expenseCollection = database.collection("expenses");
+
+    // expenses post method
+    app.post("/api/expenses", async (req, res) => {
+      const expenseData = req.body;
+
+      const result = await expenseCollection.insertOne(expenseData);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
